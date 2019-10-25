8bcb7c8ff48026b99b54f69ddb52196d1daffa74
peeraphonza2143 <53931459+peeraphonza2143@users.noreply.github.com>
2019-10-25 10:43:23 +0700

    p028

--- 61030040/ConsoleApp1/ConsoleApp1/Program.cs
+++ 61030040/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,10 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0:#.###}.", 1234.56789);
+            char ch;
+            Console.Write("Press a key followed by ENTER:");
+            ch = (char)Console.Read();  // get a char
+            Console.WriteLine("Your key is: " + ch);
 
             Console.ReadKey();
         }
