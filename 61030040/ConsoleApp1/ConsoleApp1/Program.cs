deea256a6e834f83408b9bba61c56090c321bb01
peeraphonza2143 <53931459+peeraphonza2143@users.noreply.github.com>
2019-10-25 09:53:26 +0700

    Commit branchW04P01 - branchW04P07

---
+++ 61030040/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is text 1.");
+            Console.WriteLine("This is text 2.");
+            Console.WriteLine("This is text 3.");
+            Console.ReadKey();
+        }
+    }
+}
